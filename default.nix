433935e118b38cd8d43280cf883c931ad2737478
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-05-30 21:27:59 +0800

    :bug: Remove overlays

--- default.nix
+++ default.nix
@@ -6,23 +6,32 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> { overlays = import ./overlays; } }:
+{ nixpkgs ? import <nixpkgs> { } }:
+let
+  allPkgs = nixpkgs // pkgs;
+  callPackage = path: overrides:
+    let f = import path;
+    in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
+  pkgs = rec {
+    # The `lib`, `modules`, and `overlay` names are special
+    lib = import ./lib { inherit pkgs; }; # functions
+    modules = import ./modules; # NixOS modules
+    overlays = import ./overlays; # nixpkgs overlays
 
-rec {
-  # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
+    mySources = callPackage ./_sources/generated.nix { };
 
-  help2man = pkgs.callPackage ./pkgs/development/python-modules/help2man { };
-  setuptools-generate = pkgs.callPackage ./pkgs/development/python-modules/setuptools-generate {
-    help2man = help2man;
+    help2man = callPackage ./pkgs/development/python-modules/help2man { };
+    setuptools-generate = callPackage ./pkgs/development/python-modules/setuptools-generate {
+      help2man = help2man;
+    };
+    repl-python-wakatime = callPackage ./pkgs/development/python-modules/repl-python-wakatime { };
+    translate-shell = callPackage ./pkgs/development/python-modules/translate-shell {
+      setuptools-generate = setuptools-generate;
+      repl-python-wakatime = repl-python-wakatime;
+    };
+    mulimgviewer = callPackage ./pkgs/development/python-modules/mulimgviewer {
+      # setuptools-generate = setuptools-generate;
+    };
   };
-  repl-python-wakatime = pkgs.callPackage ./pkgs/development/python-modules/repl-python-wakatime { };
-  translate-shell = pkgs.callPackage ./pkgs/development/python-modules/translate-shell {
-    setuptools-generate = setuptools-generate;
-    repl-python-wakatime = repl-python-wakatime;
-  };
-  mulimgviewer = pkgs.callPackage ./pkgs/development/python-modules/mulimgviewer {
-    # setuptools-generate = setuptools-generate;
-  };
-}
+in
+pkgs

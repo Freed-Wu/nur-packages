06fc6ec5dc267e5b92f648b05ef8f1dd74d35079
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-08-04 16:27:44 +0800

    :package: Add pyrime

---
+++ pkgs/development/python-modules/pyrime/default.nix
@@ -0,0 +1,36 @@
+{ mySources
+, python3
+, librime
+, pkg-config
+, stdenv
+, lib
+}:
+
+with python3.pkgs;
+
+buildPythonPackage rec {
+  inherit (mySources.pyrime) pname version src;
+  format = "pyproject";
+  disabled = pythonOlder "3.6";
+  propagatedBuildInputs = [
+    librime
+    colorama
+    ptpython
+  ];
+  nativeBuildInputs = [
+    meson-python
+    pkg-config
+    stdenv.cc
+  ];
+  pythonImportsCheck = [
+    "pyrime"
+  ];
+
+  meta = with lib; {
+    homepage = "https://pyrime.readthedocs.io";
+    description = "rime for python, attached to prompt-toolkit keybindings for some prompt-toolkit applications such as ptpython";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ Freed-Wu ];
+    platforms = platforms.unix;
+  };
+}
